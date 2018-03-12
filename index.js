99fd1ba5e253d4243591449ce8337705f9949f15
Duane Felton <dfelton2010@gmail.com>
2018-03-12 17:32:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var animal = 'dog'
+const two = 2
 
 function myAnimal() {
   return animal
@@ -15,7 +16,7 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
